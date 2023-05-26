022878e4aef6f97e9441c5c5a743075de8d414ed
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2023-05-26 21:00:35 +0600

    add

---
+++ README.md
@@ -0,0 +1,23 @@
+# Welcome to Chef Krunch App
+This Projects is developed by Forida Parven Anne
+
+# Technology Used on this projects
+1. Cors
+2. Express JS
+3. Node.JS
+4. Context API
+5. React Router
+7. Toastify
+8. 
+9. 
+
+## Vercel Link
+ https://shikho-learning-app-server.vercel.app
+
+## Live Site Link
+https://shikho-app.web.app        
+
+
+## Github Link
+Client Side: 
+Server Side: 
--- index.js
+++ index.js
@@ -3,33 +3,17 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const chefcards = require('./Data/chefcards.json');
 const chefrecipies = require('./Data/chefrecipies.json');
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('chef krunch is running')
 });
 
-app.get('/chefcards', (req,res) => {
-    res.send(chefcards);
-})
-
 app.get('/chefrecipies', (req,res) => {
     res.send(chefrecipies);
-})
-
-app.get('/chefcards/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedRecipe = chefcards.find(chefcards => chefcards.id === id);
-    console.log(selectedRecipe);
-})
-
-app.get('/chefrecipies/:recipeId', (req,res)=> {
-    const id = req.params.id;
-    const categoryRecipe = chefcards.filter(chefcards => chefcards.recipeId === id);
-    console.log(categoryRecipe);
 })
 
 app.listen(port, ()=> {
