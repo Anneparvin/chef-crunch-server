8a3a0b0b6ac589d26f8616fb44a73354496a8d24
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2023-05-26 16:54:14 +0600

    add5

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require ('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefcards = require('./Data/chefcards.json');
+const chefrecipies = require('./Data/chefrecipies.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef krunch is running')
